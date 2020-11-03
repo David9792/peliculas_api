da972ee503625823242f8ce3c323406359ca6e37
david9792 <david.el.unicodj@gmail.com>
2020-11-03 10:05:55 -0400

    actualizando modelo

--- src/models/movie.js
+++ src/models/movie.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const MovieSchema = new Schema({
-    title: {type: String, required: true},
+    title: {type: String, unique: true, required: true},
     description: {type: String, required: true},
-    cover_photo: {type: String, required: true},
-    main_photo: {type: String, required: true},
+    cover_photo: {type: String, required: false},
+    main_photo: {type: String, required: false},
     synopsis: {type: String, required: true},
     language: {type: String, required: true},
     rating: {type: Number, required: true},
-    list_servers: {type: [String], required: true}
+    list_servers: {type: [String], required: false},
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Movie', MovieSchema);
