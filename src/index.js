8118f661745651b9fceb0d27a334eb52b68f2766
david9792 <david.el.unicodj@gmail.com>
2020-11-03 10:35:11 -0400

    agregando rutas subir imagenes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const _ = require('lodash');
 const path = require('path');
 
 const { mongoose } = require('./database');
+
 const app = express();
 
 //settings
 app.set('port', process.env.PORT || 3000);
 
 //middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
-app.use(express.json());
+
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
 
 //routes
 app.use('/api/movies/', require('./routes/movie.routes'));
--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -5,32 +5,124 @@ const router = express.Router();
 const Movie = require('../models/movie');
 
 router.get('/', async (req, res) => {
-    const movies = await Movie.find();
-    await res.json({movies});
+    try {
+        const movies = await Movie.find();
+        await res.json({movies});
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 router.post('/', async (req, res) => {
-    const {title, description, cover_photo, main_photo, synopsis, language, rating, list_servers} = req.body;
-    const movie = new Movie({title, description, cover_photo, main_photo, synopsis, language, rating, list_servers});
-    await movie.save();
-    await res.json({status: 'movie saved'});
+    try {
+        const {title, description, synopsis, language, rating, list_servers} = req.body;
+        const movie = new Movie({title, description, synopsis, language, rating, list_servers});
+        await movie.save();
+        await res.json({status: 'movie saved'});
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 router.put('/:id', async (req, res) => {
-    const {title, description, cover_photo, main_photo, synopsis, language, rating, list_servers} = req.body;
-    const new_movie = {title, description, cover_photo, main_photo, synopsis, language, rating, list_servers};
-    await Movie.findByIdAndUpdate(req.params.id, new_movie);
-    await res.json({status: 'movie updated'});
+    try {
+        const {title, description, cover_photo, main_photo, synopsis, language, rating, list_servers} = req.body;
+        const new_movie = {title, description, cover_photo, main_photo, synopsis, language, rating, list_servers};
+        await Movie.findByIdAndUpdate(req.params.id, new_movie);
+        await res.json({status: 'movie updated'});
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 router.delete('/:id', async (req, res) => {
-    await Movie.findByIdAndRemove(req.params.id);
-    await res.json({status: 'movie deleted'});
+    try {
+        await Movie.findByIdAndRemove(req.params.id);
+        await res.json({status: 'movie deleted'});
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 router.get('/:id', async (req, res) => {
-    const movie = await Movie.findById(req.params.id);
-    await res.json({movie});
+    try {
+        const movie = await Movie.findById(req.params.id);
+        await res.json({movie});
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.post('/upload-cover-photo/:id', async (req, res) => {
+    try {
+        if (!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "cover_photo") to retrieve the uploaded file
+            let cover_photo = req.files.cover_photo;
+
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            cover_photo.mv('./uploads/cover-photos/' + cover_photo.name);
+
+            Movie.update({_id: req.params.id}, {
+                cover_photo: cover_photo.name,
+            }, function (err, affected, resp) {
+                console.log(resp);
+            })
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: cover_photo.name,
+                    mimetype: cover_photo.mimetype,
+                    size: cover_photo.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.post('/upload-main-photo/:id', async (req, res) => {
+    try {
+        if (!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "cover_photo") to retrieve the uploaded file
+            let main_photo = req.files.main_photo;
+
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            main_photo.mv('./uploads/main-photos/' + main_photo.name);
+
+            Movie.update({_id: req.params.id}, {
+                main_photo: main_photo.name,
+            }, function (err, affected, resp) {
+                console.log(resp);
+            })
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: main_photo.name,
+                    mimetype: main_photo.mimetype,
+                    size: main_photo.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 module.exports = router;
